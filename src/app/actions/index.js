efa8d45d594245634682633ee538017aebc6eba9
JulianBruxelle <bruxelle.julian@wanadoo.fr>
2019-04-16 19:43:29 +0200

    Chart

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -1,3 +1,5 @@
+import Chart from 'chart.js'
+
 const API_URL = 'https://pokeapi.co/api/v2/'
 
 const POKEMON_PATH = 'pokemon/'
@@ -7,7 +9,74 @@ const getURL = (url) => fetch(url).then(response => response.json()).catch((erro
 const get = (query) => getURL(API_URL + query)
 
 export const utils = {
-  titleCase: (string) => string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+  titleCase: (string) => string.charAt(0).toUpperCase() + string.slice(1).toLowerCase(),
+  flip: (array) => array.reduce((accumulator, current) => ([current, ...accumulator]), []),
+  color: (value) => {
+    const hue = ((value / 200) * 1.1 * 120).toString(10)
+    return ['hsl(', hue, ',100%,50%)'].join('')
+  },
+  chart: ({
+    id,
+    labels,
+    data
+  }) => {
+    const ctx = document.getElementById(id)
+    const chart = new Chart(ctx, {
+      type: 'horizontalBar',
+      data: {
+        labels: utils.flip(labels),
+        datasets: [{
+          label: 'Stats',
+          data: utils.flip(data),
+          backgroundColor: utils.flip(data).map(value => utils.color(value)),
+          borderWidth: 1
+        }]
+      },
+      options: {
+        tooltips: {
+          enabled: false
+        },
+        events: false,
+        animation: {
+          duration: 1,
+          onComplete: () => {
+            chart.datasets.forEach(function (dataset) {
+              dataset.bars.forEach(function (bar) {
+                ctx.fillText(bar.value, bar.x, bar.y - 5)
+              })
+            })
+          }
+        },
+        responsive: true,
+        legend: {
+          position: 'bottom'
+        },
+        hover: {
+          mode: 'label'
+        },
+        scales: {
+          xAxes: [{
+            display: false,
+            ticks: {
+              beginAtZero: true,
+              steps: 10,
+              stepValue: 5,
+              max: 200
+            }
+          }],
+          yAxes: [{
+            display: true,
+            ticks: {
+              beginAtZero: true,
+              steps: 10,
+              stepValue: 5,
+              max: 100
+            }
+          }]
+        }
+      }
+    })
+  }
 }
 
 export const actions = {
@@ -16,17 +85,29 @@ export const actions = {
     get(POKEMON_PATH + '?limit=10').then(response => {
       console.log(response.results)
       response.results.map((pokemon, index) => {
-        actions.getPokemon({id: pokemon.name, location: 'pokemon'})
-        actions.setPokedexPokemon({source: state.pokemon, index: index})
+        actions.getPokemon({
+          id: pokemon.name,
+          location: 'pokemon'
+        })
+        actions.setPokedexPokemon({
+          source: state.pokemon,
+          index: index
+        })
       })
     })
   },
-  getPokemon: ({id, location}) => (state, actions) => {
+  getPokemon: ({
+    id,
+    location
+  }) => (state, actions) => {
     get(POKEMON_PATH + id.toLowerCase()).then(pokemon => {
       get(POKEMON_SPECIES_PATH + id.toLowerCase()).then(species => {
         actions.set({
           entry: location,
-          data: {...pokemon, ...species}
+          data: {
+            ...pokemon,
+            ...species
+          }
         })
       })
     })
@@ -48,7 +129,9 @@ export const actions = {
     console.log('source in pokepoke', source)
     return ({
       ...state,
-      pokedex: Object.assign([...state.pokedex], {[index]: source})
+      pokedex: Object.assign([...state.pokedex], {
+        [index]: source
+      })
     })
   }
 }
--- src/app/components/Pokemon.js
+++ src/app/components/Pokemon.js
@@ -1,79 +1,81 @@
 import { h } from 'hyperapp'
 import { utils } from '../actions/index'
 import { strengths, weaknesses } from '../../../assets/types'
+
 export default ({data}) => {
-  return (
-    <pokemon>
+  if (data) {
+    return (
+      <pokemon>
+        <strengths class='data-box'>
+          <h2> Strengths </h2>
+          {
+            data && data.types && strengths(data.types.map(entry => entry.type.name)).map(strength => <p> {strength} </p>)
+          }
+        </strengths>
 
-      <strengths class='data-box'>
-        <h2> Strengths </h2>
-        {
-          data && data.types && strengths(data.types.map(entry => entry.type.name)).map(strength => <p> {strength} </p>)
+        <identity class='data-box'>
+          <h2> {data && data.name && '#' + data.id + ' ' + utils.titleCase(data.name)} </h2>
+
+          <sprites>
+            <img src={data && data.sprites && data.sprites.front_default}></img>
+            <img src={data && data.sprites && data.sprites.front_shiny}></img>
+          </sprites>
+
+          <types>
+            <h3> Types </h3>
+            {data && data.types && data.types.map((entry) =>
+              <type>
+                <p>{utils.titleCase(entry.type.name)}</p>
+              </type>
+            )}
+          </types>
+        </identity>
+
+        <weaknesses class='data-box'>
+          <h2> Weaknesses </h2>
+          {
+            data && data.types && weaknesses(data.types.map(entry => entry.type.name)).map(weakness => <p> {weakness} </p>)
+          }
+        </weaknesses>
+
+        <div class='data-box'>
+          <experience>
+            <h3> Base Experience : {data.base_experience} </h3>
+          </experience>
+
+          <hapiness>
+            <h3> Base Hapiness : {data.base_happiness + '/255'}</h3>
+          </hapiness>
+
+          <capture>
+            <h3> Capture Rate : {(100 * data.base_happiness / 255).toFixed(2) + ' %'} </h3>
+          </capture>
+
+          <gender>
+            <h3> Gender Rate : {(100 * data.gender_rate / 8).toFixed(2) + ' % female'} </h3>
+          </gender>
+
+          <abilities>
+            <h3> Abilities </h3>
+            {data && data.abilities && data.abilities.map((entry) =>
+              <ability>
+                <p>{utils.titleCase(entry.ability.name)}</p>
+              </ability>
+            )}
+          </abilities>
+        </div>
+
+        { () => data && data.stats && (
+          <canvas class='data-box' id='stats' oncreate={() => utils.chart(
+            {
+              id: 'stats',
+              labels: data.stats.map(stats => stats.stat.name),
+              data: data.stats.map(stats => stats.base_stat)
+            }
+          )}>
+          </canvas>)
         }
-      </strengths>
-
-      <identity class='data-box'>
-        <h2> {data && data.name && '#' + data.id + ' ' + utils.titleCase(data.name)} </h2>
-
-        <sprites>
-          <img src={data && data.sprites && data.sprites.front_default}></img>
-          <img src={data && data.sprites && data.sprites.front_shiny}></img>
-        </sprites>
-
-        <types>
-          <h3> Types </h3>
-          {data && data.types && data.types.map((entry) =>
-            <type>
-              <p>{utils.titleCase(entry.type.name)}</p>
-            </type>
-          )}
-        </types>
-      </identity>
-
-      <weaknesses class='data-box'>
-        <h2> Weaknesses </h2>
-        {
-          data && data.types && weaknesses(data.types.map(entry => entry.type.name)).map(weakness => <p> {weakness} </p>)
-        }
-      </weaknesses>
-
-      <div class='data-box'>
-        <experience>
-          <h3> Base Experience : {data.base_experience} </h3>
-        </experience>
-
-        <hapiness>
-          <h3> Base Hapiness : {data.base_happiness + '/255'}</h3>
-        </hapiness>
-
-        <capture>
-          <h3> Capture Rate : {(100 * data.base_happiness / 255).toFixed(2) + ' %'} </h3>
-        </capture>
-
-        <gender>
-          <h3> Gender Rate : {(100 * data.gender_rate / 8).toFixed(2) + ' % female'} </h3>
-        </gender>
-
-        <abilities>
-          <h3> Abilities </h3>
-          {data && data.abilities && data.abilities.map((entry) =>
-            <ability>
-              <p>{utils.titleCase(entry.ability.name)}</p>
-            </ability>
-          )}
-        </abilities>
-      </div>
-
-      <stats class='data-box'>
-        <h3> Stats </h3>
-        {data && data.stats && data.stats.map((entry) =>
-          <stat>
-            <h4> {entry && entry.stat.name && utils.titleCase(entry.stat.name)} </h4>
-            <p> Base Stat : {entry && entry.base_stat}</p>
-            <p> Effort : {entry && entry.effort}</p>
-          </stat>
-        )}
-      </stats>
-    </pokemon>
-  )
+      </pokemon>
+    )
+  }
 }
