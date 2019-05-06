3f923e0d123d3ab4dae2d457d930c60c4e8d5e8e
JulianBruxelle <bruxelle.julian@wanadoo.fr>
2019-05-06 10:33:00 +0200

    ff

--- src/app/components/Pokemon.js
+++ src/app/components/Pokemon.js
@@ -3,8 +3,8 @@ import { utils } from '../actions/index'
 import { strengths, weaknesses, colors } from '../../../assets/types'
 import { Link } from '@hyperapp/router'
 
-const getPokemonMainColor = (pokemon) => pokemon.types && pokemon.types.length > 0 && colors[pokemon.types[0].type.name].light
-const getPokemonSecondColor = (pokemon) => (pokemon.types && pokemon.types.length > 1 && colors[pokemon.types[1].type.name].light) || getPokemonMainColor(pokemon)
+const getPokemonMainColor = (pokemon) => pokemon && pokemon.types && pokemon.types.length > 0 && colors[pokemon.types[0].type.name].light
+const getPokemonSecondColor = (pokemon) => (pokemon && pokemon.types && pokemon.types.length > 1 && colors[pokemon.types[1].type.name].light) || getPokemonMainColor(pokemon)
 
 export default({match, data, getPokemon, setTeamOverlay}) =>
 
@@ -17,23 +17,23 @@ export default({match, data, getPokemon, setTeamOverlay}) =>
         <h2>
           Strengths
         </h2>
-        {data.types && strengths(data.types.map(entry => entry.type.name)).map(strength => <type style={'background-color:' + colors[strength].dark}>{strength.toUpperCase()}</type>)}
+        {data && data.types && strengths(data.types.map(entry => entry.type.name)).map(strength => <type style={'background-color:' + colors[strength].dark}>{strength.toUpperCase()}</type>)}
       </strengths>
       <identity class='data-box' style={'background-color:' + getPokemonMainColor(data)}>
         <h2>
-          {data.name && '#' + data.id + ' ' + utils.titleCase(data.name)}
+          {data && data.name && '#' + data.id + ' ' + utils.titleCase(data.name)}
         </h2>
 
         <sprites>
-          <img class='pokemon-image' src={data.sprites && data.sprites.front_default}></img>
-          <img class='pokemon-image' src={data.sprites && data.sprites.front_shiny}></img>
+          <img class='pokemon-image' src={data && data.sprites && data.sprites.front_default}></img>
+          <img class='pokemon-image' src={data && data.sprites && data.sprites.front_shiny}></img>
         </sprites>
         <types class='types'>
           <h3>
             Types
           </h3>
           <div id="typeList">
-            {data.types && data.types.map((entry) =>
+            {data && data.types && data.types.map((entry) =>
               <type style={'background-color:' + colors[entry.type.name].dark}>
                 {entry.type.name.toUpperCase()}
               </type>
@@ -47,49 +47,10 @@ export default({match, data, getPokemon, setTeamOverlay}) =>
         <h2>
           Weaknesses
         </h2>
-        {data.types && weaknesses(data.types.map(entry => entry.type.name)).map(weakness => <type style={'background-color:' + colors[weakness].dark}>{weakness.toUpperCase()}</type>)}
+        {data && data.types && weaknesses(data.types.map(entry => entry.type.name)).map(weakness => <type style={'background-color:' + colors[weakness].dark}>{weakness.toUpperCase()}</type>)}
       </weaknesses>
     </div>
     <div id="line2">
-      <div class='data-box' style={'background-color:' + getPokemonSecondColor(data)}>
-        <h2>
-          Attacks
-        </h2>
-        <experience>
-          <h3>
-            Base Experience : {data.experience}
-          </h3>
-        </experience>
-
-        <hapiness>
-          <h3>
-            Base Hapiness : {data.happiness + '/255'}</h3>
-        </hapiness>
-
-        <capture>
-          <h3>
-            Capture Rate : {(100 * data.gender / 255).toFixed(2) + ' %'}
-          </h3>
-        </capture>
-
-        <gender>
-          <h3>
-            Gender Rate : {(100 * data.gender / 8).toFixed(2) + ' % female'}
-          </h3>
-        </gender>
-
-        <abilities>
-          <h3>
-            Abilities
-          </h3>
-          {data.abilities && data
-            .abilities
-            .map((entry) => <ability>
-              <p>{utils.titleCase(entry.ability.name)}</p>
-            </ability>)}
-        </abilities>
-      </div>
-
       <div class='chart-container data-box' style={'position: relative; width:50vw; background-color:' + getPokemonSecondColor(data)}>
         <h2>
           Statistics
